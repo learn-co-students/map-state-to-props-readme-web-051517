14ccef2db537c54a1acf4f8aa942f0905a10aacf
Amanda <alyz26@me.com>
2017-08-04 13:27:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './App.css';
 
 class App extends Component {
@@ -12,13 +13,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
+        <button onClick={() => this.handleOnClick()}>
           Click
         </button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
+}
 
-export default App;
+const mapStateToProps = (state) => { return { items: state.items } }
+
+export default connect(mapStateToProps)(App)
