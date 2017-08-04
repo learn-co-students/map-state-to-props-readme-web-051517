271d2a3b3ad52bca632da17722ac1f369dfda1ff
veranyc <veranyc@gmail.com>
2017-08-04 14:01:01 -0400

    loveredux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -10,7 +11,12 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+//need to wrap the overall app with the Provider component from the react-redux
+//library and then pass the store to it and the App
+
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+  <App store={store}/>
+</Provider>,
   document.getElementById('root')
 );
